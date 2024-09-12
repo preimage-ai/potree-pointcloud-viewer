c7b06b05405d56fe974eb67963689e8468193638
a-ZINC <azinc28@gmail.com>
2024-09-12 11:10:02 +0530

    added promise for ifc loader

--- src/modules/IFC/ifcLoader.js
+++ src/modules/IFC/ifcLoader.js
@@ -10,24 +10,26 @@ export class IFC {
 	}
 
 	load(url, scene, name, visible = true){
-        console.log("inside ifc load", this.viewer)
-		this.ifcLoader.load(url, (model) => {
-            console.log("ifc loaded");
-
-			model.mesh.rotateX(Math.PI * 0.5);
-			// model.scale.multiplyScalar(0.3048);
-			model.mesh.name = name;
-
-			const directionalLight1 = new THREE.DirectionalLight(0xffeeff, 0.8);
-			directionalLight1.position.set(1, 1, 1);
-
-			const directionalLight2 = new THREE.DirectionalLight(0xffffff, 0.8);
-			directionalLight2.position.set(- 1, 0.5, - 1);
-			const ambientLight = new THREE.AmbientLight(0xffffee, 0.25);
-			
-			scene.scene.add(ambientLight,directionalLight1, directionalLight2, model.mesh);
-			model.visible = visible;
-			scene.addIfc(model.mesh);
+		return new Promise((resolve, reject) => {
+			this.ifcLoader.load(url, (model) => {
+				console.log("ifc loaded");
+	
+				model.mesh.rotateX(Math.PI * 0.5);
+				// model.scale.multiplyScalar(0.3048);
+				model.mesh.name = name;
+	
+				const directionalLight1 = new THREE.DirectionalLight(0xffeeff, 0.8);
+				directionalLight1.position.set(1, 1, 1);
+	
+				const directionalLight2 = new THREE.DirectionalLight(0xffffff, 0.8);
+				directionalLight2.position.set(- 1, 0.5, - 1);
+				const ambientLight = new THREE.AmbientLight(0xffffee, 0.25);
+				
+				scene.scene.add(ambientLight,directionalLight1, directionalLight2, model.mesh);
+				model.visible = visible;
+				scene.addIfc(model.mesh);
+				resolve(model.mesh);
+			}, undefined, (error) => resolve(null));
 		});
 	};
 }
