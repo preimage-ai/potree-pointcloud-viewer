09f6a987c429b05d9138374cd9571d841164d4db
a-ZINC <azinc28@gmail.com>
2025-04-29 17:35:04 +0530

    some changes in map function to make ui for ptcld to look like vtour

--- src/viewer/map.js
+++ src/viewer/map.js
@@ -1039,8 +1039,8 @@ createLineFeatures(group, transform, layer) {
 
   changeMiniMapPosition(top, left, size, url, center, width, height) {
     //console.log("this is called how many tiems ");
-    this.elMap.css("top", `${top / 2}vh`);
-    this.elMap.css("left", `${left / 2}vw`);
+    if (top !== undefined) this.elMap.css("top", `${top / 2}vh`);
+    if (left !== undefined) this.elMap.css("left", `${left / 2}vw`);
     this.elMap.css("width", `${size}px`);
     this.elMap.css("height", `${size}px`);
     let elMapHeader = $("#potree_map_header");
@@ -1052,8 +1052,10 @@ createLineFeatures(group, transform, layer) {
     elMapHeader.css("border-radius", "4px");
     elMapHeader.css("top", "10px");
     if (this.calledOnce == true) return;
-    this.elMap.css("display", "block");
-    this.enabled = true;
+    if (this.viewer.isUnityView()) {
+      this.elMap.css("display", "block");
+      this.enabled = true;
+    }
     let view = this.map.getView();
     let extendsSize = 300;
     if (size <= 150) {
